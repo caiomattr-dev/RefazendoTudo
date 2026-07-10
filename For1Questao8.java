ae95258d75c6b87953af0e91e7a8ebd8cfd1d4d5
caio <caiomronconi@gmail.com>
2026-07-10 09:37:46 -0300

    Update For1Questao8.java

--- For1Questao8.java
+++ For1Questao8.java
@@ -6,115 +6,47 @@ public class For1Questao8 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
-		int contBomAproveitamento = 0;
-		double contExemplar = 0;
-		double percE = 0;
-		double mediaAnual = 0;
-		double a1 = 0;
-		double a2 = 0;
-		double a3 = 0;
-		double a4 = 0;
-		double a5 = 0;
+		int contBom = 0;
+		int contExemplar = 0;
+		double nota = 0;
+		String citacao = "";
+		String nome;
+		String bimestres10 = "";
 		
 		for(int i = 1 ; i <= 5 ; i++) {
-			System.out.println("Insira suas notas bimestrais: ");
-			double n1 = s.nextInt();
-			double n2 = s.nextInt();
-			double n3 = s.nextInt();
-			double n4 = s.nextInt();
-			mediaAnual = (n1 + n2 + n3 + n4) / 4;
+			double somaNota = 0;
+			int notas10 = 0;
 			
-			if(mediaAnual < 7) {
-				System.out.println("Você reprovou. ");
+			System.out.println("Informe nome ");
+			nome = s.next();
+			for(int n = 1; n <= 4 ; n++) {
+				System.out.println("Informe nota do semestre " + n);
+				nota = s.nextDouble();
+				somaNota += nota;
+				if(nota == 10) {
+					notas10++;
+					bimestres10 += n + ", ";
+				}
 			}
-			if(i == 1) {
-				a1 = mediaAnual;
-				if(mediaAnual <= 7.9 && mediaAnual >= 7) {
-					System.out.println("honras pelo bom aproveitamento");
-					contBomAproveitamento++;
-				}
-				if(mediaAnual <= 8.9 && mediaAnual >= 8) {
-					System.out.println("honras pelo ótimo aproveitamento");
-				}
-				if(mediaAnual <= 10 && mediaAnual >= 9) {
-					System.out.println("honras pelo excelente aproveitamento");
-					if(n1 == 10 && n2 ==10 || n1 == 10 && n3 == 10 || n1 == 10 && n4 == 10 || n2 == 10 && n3 == 10 || n2 == 10 && n4 == 10) {
-						System.out.println("Aluno exemplar");
+			if((somaNota/4) >= 7 && (somaNota/4) <= 7.9) {
+				contBom++;
+				citacao = "honras pelo bom aproveitamento";
+			}
+			else
+				if((somaNota/4) >= 8 && (somaNota/4) <= 8.9)
+					citacao = "honras pelo Ã³timo aproveitamento";
+				else
+					if((somaNota/4) >= 9 && (somaNota/4) <= 10 && notas10 >= 2){
 						contExemplar++;
+						citacao = "aluno exemplar.Bimestres com media 10:" + bimestres10;
 					}
-				}
-			}
-			if(i == 2) {
-				a2 = mediaAnual;
-				if(mediaAnual <= 7.9 && mediaAnual >= 7) {
-					System.out.println("honras pelo bom aproveitamento");
-					contBomAproveitamento++;
-				}
-				if(mediaAnual <= 8.9 && mediaAnual >= 8) {
-					System.out.println("honras pelo ótimo aproveitamento");
-				}
-				if(mediaAnual <= 10 && mediaAnual >= 9) {
-					System.out.println("honras pelo excelente aproveitamento");
-					if(n1 == 10 && n2 ==10 || n1 == 10 && n3 == 10 || n1 == 10 && n4 == 10 || n2 == 10 && n3 == 10 || n2 == 10 && n4 == 10) {
-						System.out.println("Aluno exemplar");
-						contExemplar++;
-					}
-				}
-			}
-			if(i == 3) {
-				a3 = mediaAnual;
-				if(mediaAnual <= 7.9 && mediaAnual >= 7) {
-					System.out.println("honras pelo bom aproveitamento");
-					contBomAproveitamento++;
-				}
-				if(mediaAnual <= 8.9 && mediaAnual >= 8) {
-					System.out.println("honras pelo ótimo aproveitamento");
-				}
-				if(mediaAnual <= 10 && mediaAnual >= 9) {
-					System.out.println("honras pelo excelente aproveitamento");
-					if(n1 == 10 && n2 ==10 || n1 == 10 && n3 == 10 || n1 == 10 && n4 == 10 || n2 == 10 && n3 == 10 || n2 == 10 && n4 == 10) {
-						System.out.println("Aluno exemplar");
-						contExemplar++;
-					}
-				}
-			}
-			if(i == 4) {
-				a4 = mediaAnual;
-				if(mediaAnual <= 7.9 && mediaAnual >= 7) {
-					System.out.println("honras pelo bom aproveitamento");
-					contBomAproveitamento++;
-				}
-				if(mediaAnual <= 8.9 && mediaAnual >= 8) {
-					System.out.println("honras pelo ótimo aproveitamento");
-				}
-				if(mediaAnual <= 10 && mediaAnual >= 9) {
-					System.out.println("honras pelo excelente aproveitamento");
-					if(n1 == 10 && n2 ==10 || n1 == 10 && n3 == 10 || n1 == 10 && n4 == 10 || n2 == 10 && n3 == 10 || n2 == 10 && n4 == 10) {
-						System.out.println("Aluno exemplar");
-						contExemplar++;
-					}
-				}
-			}
-			if(i == 5) {
-				a5 = mediaAnual;
-				if(mediaAnual <= 7.9 && mediaAnual >= 7) {
-					System.out.println("honras pelo bom aproveitamento");
-					contBomAproveitamento++;
-				}
-				if(mediaAnual <= 8.9 && mediaAnual >= 8) {
-					System.out.println("honras pelo ótimo aproveitamento");
-				}
-				if(mediaAnual <= 10 && mediaAnual >= 9) {
-					System.out.println("honras pelo excelente aproveitamento");
-					if(n1 == 10 && n2 ==10 || n1 == 10 && n3 == 10 || n1 == 10 && n4 == 10 || n2 == 10 && n3 == 10 || n2 == 10 && n4 == 10) {
-						System.out.println("Aluno exemplar");
-						contExemplar++;
-					}
-				}
-			}
-			System.out.println("");
+					else
+						citacao = "honras pelo excelente aproveitamento";
+			
+			System.out.println("nome == " + nome + "\nmedia anual == " + (somaNota/4) + 
+					   "\ncitacao == " + citacao + "\n");
 		}
-		percE = (contExemplar / 5) * 100;
-		System.out.println("Qntd de alunos com bom aproveitamento: " + contBomAproveitamento + ", percentual de alunos exemplares: " + percE + "%");
+		System.out.println("horas pelo bom aproveitamento == " + contBom + 
+				   "\npercentual aluno exemplar == " + ((contExemplar * 100)/50) + "%");
 	}
 }
